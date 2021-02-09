f70e7088699b400d354edf3b7e705d0cd483a8f9
Jonathon Joyner <jonathonandon@gmail.com>
2021-02-08 19:40:03 -0600

    second commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Baby_Spice_ConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
