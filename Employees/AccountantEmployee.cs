98f3a28e1276a1d43a37f47ef801fbcbb347704e
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-02-08 21:11:56 -0600

    added accountant employee class

---
+++ Employees/AccountantEmployee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baby_Spice_ConsoleProject.Employees
+{
+    class AccountantEmployee : DunderEmployees
+    {
+        
+        public AccountantEmployee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
