ef9bb1dda5db32be18710a7d6e8b73dd7cac5e7f
Michael Pfohl <rmpfohl@gmail.com>
2021-02-08 20:36:50 -0600

    add sales employee class

---
+++ Employees/SalesEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Baby_Spice_ConsoleProject;
+
+namespace Baby_Spice_ConsoleProject.Employees
+{
+    class SalesEmployee : DunderEmployees
+    {
+        public List<Sale> Sales { get; set; }
+
+        public SalesEmployee(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
