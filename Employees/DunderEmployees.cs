067e4f0ce69dfb65c3a83bab95ff0dd02c0ac6e4
Jonathon Joyner <jonathonandon@gmail.com>
2021-02-08 20:15:14 -0600

    added changes

--- Employees/DunderEmployees.cs
+++ Employees/DunderEmployees.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Baby_Spice_ConsoleProject.Employees
 {
-    class dunderEmployees
+    class DunderEmployees
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DunderEmployees(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"Hi, {FirstName}");
+        }
     }
+   
 }
