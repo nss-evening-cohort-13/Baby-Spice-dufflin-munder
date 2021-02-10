650c7ea36e986e932e92f1aaae025f0b1bb1b287
Bailey <bailey.elise.dennis@gmail.com>
2021-02-09 18:43:25 -0600

    re-worked the main menu of the application

--- Program.cs
+++ Program.cs
@@ -6,36 +6,50 @@ namespace Baby_Spice_ConsoleProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Dufflin/Munder Cardboard Co. ");
-            Console.WriteLine("Sales Portal!\n");
-            Console.WriteLine("1. Enter Sales");
-            Console.WriteLine("2. Generate Report For Accountant");
-            Console.WriteLine("3. Add New Sales Employee");
-            Console.WriteLine("4. Find A Sale");
-            Console.WriteLine("5. Exit");
-
-            var running = true;
-            while (running)
+            bool showMenu = true;
+            while (showMenu)
             {
+                showMenu = MainMenu();
+            }
+
+            bool MainMenu()
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Dufflin/Munder Cardboard Co. ");
+                Console.WriteLine("Sales Portal!\n");
+                Console.WriteLine("1. Enter Sales");
+                Console.WriteLine("2. Generate Report For Accountant");
+                Console.WriteLine("3. Add New Sales Employee");
+                Console.WriteLine("4. Find A Sale");
+                Console.WriteLine("5. Exit");
+
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        Console.WriteLine("Which Sales Employee Are You?");
-                        break;
+                        showMenu = false;
+                        EnterSales();
+                        return true;
                     case "2":
                         Console.WriteLine("Generate A Report");
-                        break;
+                        return true; ;
                     case "3":
                         Console.WriteLine("Update me");
-                        break;
+                        return true;
                     case "4":
                         Console.WriteLine("Enter Sale Info");
-                        break;
+                        return true;
                     case "5":
-                        Console.WriteLine("Beat it, Bozo!");
-                        running = false;
-                        break;
+                        return false;
+                    default:
+                        return true;
                 }
+            }
+            void EnterSales()
+            {
+                Console.WriteLine("Which Sales Employee Are You?");
+                var x = Console.ReadLine();
+                Console.WriteLine($"{x}");
+                showMenu = true;
             }
         }
     }
